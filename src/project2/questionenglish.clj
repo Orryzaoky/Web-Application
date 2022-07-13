723960d8ff0e3f2b7b8671e0fb7b1d3671edbbf0
Orryzaoky <107123554+Orryzaoky@users.noreply.github.com>
2022-07-14 00:46:18 +0700

    Update questionenglish.clj

--- src/project2/questionenglish.clj
+++ src/project2/questionenglish.clj
@@ -3,8 +3,8 @@
             [hiccup.form :as form]
             [ring.util.anti-forgery :refer (anti-forgery-field)]))
 
-(defn soaleng [soal]
-   (for [i (range 8)]
+(defn soaleng [soal] 
+  (for [i (range 8)]
    [:div
     [:h2 (str (inc i) "/8")]
     [:br]
@@ -20,16 +20,19 @@
     (form/label (str "no" i) (str "   " (get-in soal [i :soal :options 2 1])))
     [:br]
     (form/radio-button (str "no" i) false "D")
-    (form/label (str "no" i) (str "   " (get-in soal [i :soal :options 3 1])))]))
+    (form/label (str "no" i) (str "   " (get-in soal [i :soal :options 3 1])))]
+    ))
 
 (defn soalinggris [soal]
   (html5
    [:head [:title "QuestionEnglish"]
     (include-css "questionenglish.css")]
-   [:body [:h1 "Bahasa Inggris"]
+   [:body [:h1 "English"]
     (form/form-to
      [:post "/quizinggris"]
-    (soaleng soal)
+     (form/hidden-field "materi" (get-in soal [0 :topic]))
+     (form/hidden-field "problems" soal)
+     (soaleng soal)
      [:br]
      (anti-forgery-field)
      [:h1 (form/submit-button "SUBMIT")])]))
